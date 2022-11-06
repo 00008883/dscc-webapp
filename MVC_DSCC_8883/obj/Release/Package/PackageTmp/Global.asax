dfa14b7eeeaf7f60b1a751ef5483b90a3861235d
00008883 <solimjonov@students.wiut.uz>
2022-11-06 16:10:51 +0500

    Adding publish app files and scripts

---
+++ MVC_DSCC_8883/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_DSCC_8883.MvcApplication" Language="C#" %>
---
+++ installwebsite.bat
@@ -0,0 +1,5 @@
+%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy Unrestricted -Force"
+
+cd c:\temp
+
+%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\installwebsite.ps1"
