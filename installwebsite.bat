9ff9d6487cd564ee7db1cb7f637383e41c712679
00008883 <solimjonov@students.wiut.uz>
2022-11-06 16:28:07 +0500

    fix bugs

--- installwebsite.bat
+++ installwebsite.bat
@@ -1,5 +1,7 @@
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy Unrestricted -Force"
 
+IF NOT EXIST c:\inetpub\wwwroot\webtest mkdir c:\inetpub\wwwroot\MVCPublish
+
 cd c:\temp
 
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\installwebsite.ps1"
